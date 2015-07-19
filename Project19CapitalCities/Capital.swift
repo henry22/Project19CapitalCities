72c1a0e0acbea723fc5cd1ecc32a150dfe521c6a
henry22 <henry811010@gmail.com>
2015-07-19 23:23:32 +0800

    With map annotations, we can't use structs, and we must inherit from NSObject because it needs to interactive with Apple's Objective C code.

---
+++ Project19CapitalCities/Capital.swift
@@ -0,0 +1,13 @@
+//
+//  Capital.swift
+//  Project19CapitalCities
+//
+//  Created by Henry on 7/19/15.
+//  Copyright (c) 2015 Henry. All rights reserved.
+//
+
+import UIKit
+
+class Capital: NSObject {
+   
+}
